8556b6845de2d6d0cd5ccec6c15075336554e0e5
Adnan Nazir <adnannazir.cs@gmail.com>
2024-01-23 10:39:02 +0500

    Initial commit

---
+++ customers/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django_tenants.models import TenantMixin, DomainMixin
+
+from tenant_users.tenants.models import TenantBase
+
+class Client(TenantBase):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.schema_name
+
+
+class Domain(DomainMixin):
+    pass
